11df8f20bc2fbdd38e29a6efd5c1eb73cea9c7a0
Rick Casey <rick.casey@gmail.com>
2012-09-24 01:57:43 -0500

    Formatting

--- src/index.template.php
+++ src/index.template.php
@@ -1,76 +1,16 @@
 <?php
-/**
- * Simple Machines Forum (SMF)
- *
- * @package SMF
- * @author Simple Machines
- * @copyright 2011 Simple Machines
- * @license http://www.simplemachines.org/about/smf/license.php BSD
- *
- * @version 2.0
- */
-
-/*	This template is, perhaps, the most important template in the theme. It
-	contains the main template layer that displays the header and footer of
-	the forum, namely with main_above and main_below. It also contains the
-	menu sub template, which appropriately displays the menu; the init sub
-	template, which is there to set the theme up; (init can be missing.) and
-	the linktree sub template, which sorts out the link tree.
-
-	The init sub template should load any data and set any hardcoded options.
-
-	The main_above sub template is what is shown above the main content, and
-	should contain anything that should be shown up there.
-
-	The main_below sub template, conversely, is shown after the main content.
-	It should probably contain the copyright statement and some other things.
-
-	The linktree sub template should display the link tree, using the data
-	in the $context['linktree'] variable.
-
-	The menu sub template should display all the relevant buttons the user
-	wants and or needs.
-
-	For more information on the templating system, please see the site at:
-	http://www.simplemachines.org/
-*/
-
-// Initialize the template... mainly little settings.
 function template_init()
 {
 	global $context, $settings, $options, $txt, $twig, $sourcedir;
 
-	/* Use images from default theme when using templates from the default theme?
-		if this is 'always', images from the default theme will be used.
-		if this is 'defaults', images from the default theme will only be used with default templates.
-		if this is 'never' or isn't set at all, images from the default theme will not be used. */
 	$settings['use_default_images'] = 'never';
-
-	/* What document type definition is being used? (for font size and other issues.)
-		'xhtml' for an XHTML 1.0 document type definition.
-		'html' for an HTML 4.01 document type definition. */
 	$settings['doctype'] = 'xhtml';
-
-	/* The version this template/theme is for.
-		This should probably be the version of SMF it was created for. */
 	$settings['theme_version'] = '2.0';
-
-	/* Set a setting that tells the theme that it can render the tabs. */
 	$settings['use_tabs'] = true;
-
-	/* Use plain buttons - as opposed to text buttons? */
 	$settings['use_buttons'] = true;
-
-	/* Show sticky and lock status separate from topic icons? */
 	$settings['separate_sticky_lock'] = true;
-
-	/* Does this theme use the strict doctype? */
 	$settings['strict_doctype'] = false;
-
-	/* Does this theme use post previews on the message index? */
 	$settings['message_index_preview'] = false;
-
-	/* Set the following variable to true if this theme requires the optional theme strings file to be loaded. */
 	$settings['require_theme_strings'] = false;
 
 	require_once $sourcedir . '/libs/Twig/Autoloader.php';
@@ -83,46 +23,44 @@ function template_init()
 	));
 }
 
-// The main sub template above the content.
 function template_html_above()
 {
 	global $context, $settings, $options, $scripturl, $txt, $modSettings, $twig;
 
 	$vm = array(
 		'settings' => array(
-			'defaultThemeUrl' => $settings['default_theme_url'],
-			'imagesUrl' => $settings['images_url'],
-			'scriptUrl' => $scripturl,
-			'themeUrl' => $settings['theme_url'],
-			'themeVariant' => $settings['theme_variant'],
+			'defaultThemeUrl' 	=> $settings['default_theme_url'],
+			'imagesUrl' 		=> $settings['images_url'],
+			'scriptUrl' 		=> $scripturl,
+			'themeUrl' 			=> $settings['theme_url'],
+			'themeVariant' 		=> $settings['theme_variant'],
 			),
 		'html' => array(
-			'showRtlDir' => (($context['right_to_left']) ? true : false),
+			'showRtlDir' 	=> (($context['right_to_left']) ? true : false),
 			),
 		'head' => array(
-			'canonicalUrl' => !empty($context['canonical_url']),
-			'characterSet' => $context['character_set'], 
-			'cssFix' => null,
-			'currentBoard' => $context['current_board'],
-			'currentTopic' => $context['current_topic'],
-			'forumName' => $context['forum_name_html_safe'],
-			'headers' => $context['html_headers'],
-			'isoCaseFolding' => $context['server']['iso_case_folding'] ? 'true' : 'false',
-			'metaKeywords' => $context['meta_keywords'],
-			'pageTitle' => $context['page_title_html_safe'],
-			'robotNoIndex' => !empty($context['robotNoIndex']),
-			'showPMPopup' => (($context['show_pm_popup']) ? true : false),
-			'showRss' => (!empty($modSettings['xmlnews_enable']) && (!empty($modSettings['allow_guestAccess']) || $context['user']['is_logged'])),
+			'canonicalUrl' 		=> !empty($context['canonical_url']),
+			'characterSet' 		=> $context['character_set'], 
+			'cssFix' 			=> null,
+			'currentBoard' 		=> $context['current_board'],
+			'currentTopic' 		=> $context['current_topic'],
+			'forumName'			=> $context['forum_name_html_safe'],
+			'headers' 			=> $context['html_headers'],
+			'isoCaseFolding' 	=> $context['server']['iso_case_folding'] ? 'true' : 'false',
+			'metaKeywords' 		=> $context['meta_keywords'],
+			'pageTitle' 		=> $context['page_title_html_safe'],
+			'robotNoIndex' 		=> !empty($context['robotNoIndex']),
+			'showPMPopup' 		=> (($context['show_pm_popup']) ? true : false),
+			'showRss' 			=> (!empty($modSettings['xmlnews_enable']) && (!empty($modSettings['allow_guestAccess']) || $context['user']['is_logged'])),
 			),
 		'txt' => array(
-			'rss' => $txt['rss'],
-			'showPrsonalMessages' => $txt['show_personal_messages'],
-			'ajaxInProgress' => $txt['ajax_in_progress'],
-			'modifyCancel' => $txt['modify_cancel'],
+			'rss' 					=> $txt['rss'],
+			'showPrsonalMessages' 	=> $txt['show_personal_messages'],
+			'ajaxInProgress' 		=> $txt['ajax_in_progress'],
+			'modifyCancel' 			=> $txt['modify_cancel'],
 			),
 		);
 
-	// Some browsers need an extra stylesheet due to bugs/compatibility issues.
 	foreach (array('ie7', 'ie6', 'webkit') as $cssfix)
 		if ($context['browser']['is_' . $cssfix])
 			$vm['head']['cssFix'] = $cssfix;
@@ -262,12 +200,10 @@ function template_html_below()
 	echo $twig->render('index.html.below.template.html');
 }
 
-// Show a linktree. This is that thing that shows "My Community | General Category | General Discussion"..
 function theme_linktree($force_show = false)
 {
 	global $context, $settings, $options, $shown_linktree, $twig;
 
-	// If linktree is empty, just return - also allow an override.
 	if (empty($context['linktree']) || (!empty($context['dont_default_linktree']) && !$force_show))
 		return;
 
@@ -287,7 +223,6 @@ function theme_linktree($force_show = false)
 	$shown_linktree = true;
 }
 
-// Show the menu up top. Something like [home] [help] [profile] [logout]...
 function template_menu()
 {
 	global $context, $settings, $options, $scripturl, $txt, $twig;
@@ -306,7 +241,6 @@ function template_menu()
 	echo $twig->render('index.menu.template.html', $vm);
 }
 
-// Generate a strip of buttons.
 function template_button_strip($button_strip, $direction = 'top', $strip_options = array())
 {
 	global $settings, $context, $txt, $scripturl, $twig;
@@ -318,7 +252,6 @@ function template_button_strip($button_strip, $direction = 'top', $strip_options
 
 	$buttons = array();
 
-	// List the buttons in reverse order for RTL languages.
 	if ($context['right_to_left'])
 		$button_strip = array_reverse($button_strip, true);
 
@@ -347,6 +280,4 @@ function template_button_strip($button_strip, $direction = 'top', $strip_options
 		);
 
 	echo $twig->render('index.buttonstrip.template.html', $vm);
-}
-
-?>
+} ?>
