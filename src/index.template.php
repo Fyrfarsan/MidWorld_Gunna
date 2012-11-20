af909fcf183bfe888acf05d14a59ed3f9aa01be4
Rick Casey <rick.casey@gmail.com>
2012-11-20 10:01:30 -0600

    Removing SVG

--- src/index.template.php
+++ src/index.template.php
@@ -70,7 +70,7 @@ function template_html_above()
 
 function template_body_above()
 {
-	global $context, $settings, $options, $scripturl, $txt, $modSettings, $twig, $user_info, $smcFunc;
+	global $context, $settings, $options, $scripturl, $txt, $modSettings, $twig, $user_info, $smcFunc, $shown_linktree;
 
   	$primaryGroupName = '';
 
@@ -97,7 +97,55 @@ function template_body_above()
 			'isAdmin' => ($context['user']['is_admin']) ? true : false,
 			'isNewsEnabled' => (!empty($settings['enable_news'])),
 			),
-		'content' => array(
+		'context' => array(
+			'menu' => $context['menu_buttons'],
+			'icons' => array(
+				'approvemembers' => array(
+					'title' => 'Approve Members',
+					'href' => $scripturl . '?action=admin;area=viewmembers;sa=browse;type=approve',
+					'show' => 1,
+					'target' => '_self',
+					'sub_buttons' => array(),
+					'is_last' => false,
+					'level' => 0,
+					'active_button' => 0
+					),
+				'showunread' => array(
+					'title' => 'Show Unread Posts',
+					'href' => $scripturl . '?action=unread',
+					'show' => 1,
+					'target' => '_self',
+					'sub_buttons' => array(),
+					'is_last' => false,
+					'level' => 0,
+					'active_button' => 0
+					),
+				'showreplies' => array(
+					'title' => 'Show Unread Replies',
+					'href' => $scripturl . '?action=unreadreplies',
+					'show' => 1,
+					'target' => '_self',
+					'sub_buttons' => array(),
+					'is_last' => false,
+					'level' => 0,
+					'active_button' => 0
+					),
+				'pm' => $context['menu_buttons'][7],
+				'xbl' => array(
+					'title' => 'Xbox Live Status',
+					'href' => $scripturl . '?action=gsfs',
+					'show' => 1,
+					'target' => '_self',
+					'sub_buttons' => array(),
+					'is_last' => false,
+					'level' => 0,
+					'active_button' => 0
+					),
+				),
+			'login' => $context['menu_buttons'][11],
+			'register' => $context['menu_buttons'][12],
+			'logout' => $context['menu_buttons'][10],
+			'home' => $context['menu_buttons'][1],
 			'showForumWidth' => !empty($settings['forum_width']),
 			'forumWidth' => $settings['forum_width'],
 			'showHeaderLogo' => !empty($context['header_logo_url_html_safe']),
@@ -154,15 +202,35 @@ function template_body_above()
 	$vm['txt']['approvalMessagePrefix'] = ($vm['content']['unapprovedMemberCount'] == 1) ? $txt['approve_thereis'] : $txt['approve_thereare'];
 	$vm['txt']['approvalMessageCount'] = ($vm['content']['unapprovedMemberCount'] == 1) ? $txt['approve_member'] : ($context['unapproved_members'] . ' ' . $txt['approve_members']);
 
-	ob_start();
-	template_menu();
-	$vm['content']['templateMenu'] = ob_get_contents();
-	ob_end_clean();
+	if (!(empty($context['linktree']) || (!empty($context['dont_default_linktree']) && !$force_show))) {
+		$vm['settings']['linktreeLink'] = $settings['linktree_link'];
+		$vm['settings']['linktree'] = $settings['linktree'];
+		$shown_linktree = true;
+	}
+
+	unset($vm['context']['menu'][1]);
+	unset($vm['context']['menu'][2]);
+	unset($vm['context']['menu'][6]);
+	unset($vm['context']['menu'][7]);
+	unset($vm['context']['menu'][8]);
+	unset($vm['context']['menu'][10]);
+
+	unset($vm['context']['icons'][1]);
+	unset($vm['context']['icons'][2]);
+	unset($vm['context']['icons'][6]);
+	unset($vm['context']['icons'][7]);
+	unset($vm['context']['icons'][8]);
+	unset($vm['context']['icons'][10]);
 
 	ob_start();
-	theme_linktree();
-	$vm['content']['templateLinktree'] = ob_get_contents();
+	print_r($vm);
+	$vm['debug']= ob_get_contents();
 	ob_end_clean();
+
+	// ob_start();
+	// theme_linktree();
+	// $vm['content']['templateLinktree'] = ob_get_contents();
+	// ob_end_clean();
 
 	echo $twig->render('index.body.above.template.html', $vm);
 }
@@ -249,46 +317,46 @@ function template_html_below()
 	echo $twig->render('index.html.below.template.html');
 }
 
-function theme_linktree($force_show = false)
-{
-	global $context, $settings, $options, $shown_linktree, $twig;
+// function theme_linktree($force_show = false)
+// {
+// 	global $context, $settings, $options, $shown_linktree, $twig;
 
-	if (empty($context['linktree']) || (!empty($context['dont_default_linktree']) && !$force_show))
-		return;
+// 	if (empty($context['linktree']) || (!empty($context['dont_default_linktree']) && !$force_show))
+// 		return;
 
-	$vm = array(
-		'settings' => array(
-			'defaultThemeUrl' => $settings['default_theme_url'],
-			'imagesUrl' => $settings['images_url'],
-			'scriptUrl' => $scripturl,
-			'themeUrl' => $settings['theme_url'],
-			'linktreeLink' => $settings['linktree_link'],
-			),
-		'linktree' => $context['linktree'],
-		);
+// 	$vm = array(
+// 		'settings' => array(
+// 			'defaultThemeUrl' => $settings['default_theme_url'],
+// 			'imagesUrl' => $settings['images_url'],
+// 			'scriptUrl' => $scripturl,
+// 			'themeUrl' => $settings['theme_url'],
+// 			'linktreeLink' => $settings['linktree_link'],
+// 			),
+// 		'linktree' => $context['linktree'],
+// 		);
 
-	echo $twig->render('index.linktree.template.html', $vm);
+// 	echo $twig->render('index.linktree.template.html', $vm);
 
-	$shown_linktree = true;
-}
+// 	$shown_linktree = true;
+// }
 
-function template_menu()
-{
-	global $context, $settings, $options, $scripturl, $txt, $twig;
+// function template_menu()
+// {
+// 	global $context, $settings, $options, $scripturl, $txt, $twig;
 
-	$vm = array(
-		'settings' => array(
-			'defaultThemeUrl' => $settings['default_theme_url'],
-			'imagesUrl' => $settings['images_url'],
-			'scriptUrl' => $scripturl,
-			'themeUrl' => $settings['theme_url'],
-			'linktreeLink' => $settings['linktree_link'],
-			),
-		'menu' => $context['menu_buttons'],
-		);
+// 	$vm = array(
+// 		'settings' => array(
+// 			'defaultThemeUrl' => $settings['default_theme_url'],
+// 			'imagesUrl' => $settings['images_url'],
+// 			'scriptUrl' => $scripturl,
+// 			'themeUrl' => $settings['theme_url'],
+// 			'linktreeLink' => $settings['linktree_link'],
+// 			),
+// 		'menu' => $context['menu_buttons'],
+// 		);
 
-	echo $twig->render('index.menu.template.html', $vm);
-}
+// 	echo $twig->render('index.menu.template.html', $vm);
+// }
 
 function template_button_strip($button_strip, $direction = 'top', $strip_options = array())
 {
